0448347afdb51f8d3e2f4edacaf773830dde0944
porchsal <porchsal@yahoo.com>
2023-03-31 23:18:41 -0400

    iss_promised.js linted

--- iss_promised.js
+++ iss_promised.js
@@ -1,32 +1,33 @@
 const request = require('request-promise-native');
-const { fetchCoordsByIp } = require('./iss');
 
-const fetchMyIP = function () {
-    return request('https://api.ipify.org?format=json'); 
-}
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
 
 const fetchCoordsByIP = function(body) {
-    const ip = JSON.parse(body).ip;
-    return request(`http://ipwho.is/${ip}`);
-}
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
 
 
 const fetchISSFlyOverTimes = function(coord) {
-    const { latitude, longitude } = JSON.parse(coord);
-    const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
-    return request(url);
+  const { latitude, longitude } = JSON.parse(coord);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
 
-}
+};
 
 const nextISSTimesForMyLocation = function() {
-    return fetchMyIP()
+  return fetchMyIP()
     .then(fetchCoordsByIP)
     .then(fetchISSFlyOverTimes)
     .then((data) => {
-        const { response } = JSON.parse(data);
-        return response;
+      const { response } = JSON.parse(data);
+      return response;
     }
-)}
+    );
+};
 
 
 module.exports = { nextISSTimesForMyLocation };
