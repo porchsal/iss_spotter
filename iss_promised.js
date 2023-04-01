070ace696d2407f13a8228e21cb6182c4d93e2e8
porchsal <porchsal@yahoo.com>
2023-03-31 23:09:52 -0400

    iss_promised.js finished

--- iss_promised.js
+++ iss_promised.js
@@ -18,7 +18,15 @@ const fetchISSFlyOverTimes = function(coord) {
 
 }
 
+const nextISSTimesForMyLocation = function() {
+    return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+        const { response } = JSON.parse(data);
+        return response;
+    }
+)}
 
 
-
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+module.exports = { nextISSTimesForMyLocation };
