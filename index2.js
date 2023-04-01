1dbe4f58caad6218833d227bdd598f7f099882dc
porchsal <porchsal@yahoo.com>
2023-03-31 23:10:05 -0400

    index2.js finished

--- index2.js
+++ index2.js
@@ -1,6 +1,11 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+const { printPassTimes } = require('./index');
 
-fetchMyIP()
-    .then(fetchCoordsByIP)
-    .then(fetchISSFlyOverTimes)
-    .then(body => console.log(body));
+  nextISSTimesForMyLocation()
+    .then((passTimes) => {
+        printPassTimes(passTimes);
+    })
+    .catch((error) => {
+        console.log("It didn't work: ", error.message);
+    });
+
